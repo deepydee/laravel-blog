b3285d11dde109b5d86a5984c20506bbc6910199
Vitaliy Semenov <synthex@bk.ru>
2022-08-01 14:07:56 +0000

    Categories, Posts, Tags migration changed

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tag;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 
 class MainController extends Controller
 {
