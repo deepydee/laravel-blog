14f2ebd2f27d0daabb945b5b24724b67238b7dd8
Vitaliy Semenov <synthex@bk.ru>
2022-08-01 11:58:59 +0000

    Admin template added

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('public');
+
+mix.combine([
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    'resources/assets/admin/css/adminlte.min.css'
+], 'public/assets/admin/css/admin.min.css');
+
+mix.combine([
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/assets/admin/js/adminlte.min.js',
+    'resources/assets/admin/js/demo.js',
+], 'public/assets/admin/js/admin.min.js');
+
+mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
+
+mix.copy('resources/assets/admin/css/adminlte.min.css.map', 'public/assets/admin/css/adminlte.min.css.map');
+mix.copy('resources/assets/admin/js/adminlte.min.js.map', 'public/assets/admin/js/adminlte.min.js.map');
